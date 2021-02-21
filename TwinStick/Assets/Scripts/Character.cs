2234ac4529ebc63031cb1a289447dc3248242ad5
FrankiePP <sp3990@nyu.edu>
2021-02-21 18:21:29 -0500

    Basic Project structure and input in project settings

---
+++ TwinStick/Assets/Scripts/Character.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+
+    public float health = 100;
+    public float moveSpeed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void CheckInput()
+    {
+        if (Input.GetAxis("Horizontal")>0.1f)
+        {
+
+        }
+
+        if (Input.GetAxis("Vertical") > 0.1f)
+        {
+
+        }
+
+
+    }
+}
